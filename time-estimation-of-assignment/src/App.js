b64125b9d6415eb0455f5b4739d837f504204916
Abhishek Gaur <abhishekgaur1996@gmail.com>
2024-08-06 14:43:06 -0400

    "Changes in the App.js file"

--- time-estimation-of-assignment/src/App.js
+++ time-estimation-of-assignment/src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
 // 1. when we are using Firebase we don't need to manage our authentication tokens like JWT on the backend. The backend's role will be now is to handle data storage, sich as storing user responses on questions, but it won't need to handle user authentication or authorization directly.
 // 2. Now I have to make sure that my backend is reveiving and storing user responses and perhaps associated them with the user's UID(provided by Firebase) if necessary.
-import { Routes, Route, useNavigate } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import LogIn from './components/LogIn';
@@ -17,37 +17,45 @@ function App() {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
-  useEffect(()=> {
-    const unsubsribe = onAuthStateChanged( firebaseAuth, (currentUser) =>{
-      setUser(currentUser);
-      if(currentUser){
-        navigate("/");
-      }
-    });
+  // useEffect(()=> {
+  //   const unsubsribe = onAuthStateChanged( firebaseAuth, (currentUser) =>{
+  //     setUser(currentUser);
+  //     if(currentUser){
+  //       navigate("/");
+  //     }
+  //   });
 
-    return() => unsubsribe();
-  }, [navigate]);
+  //   return() => unsubsribe();
+  // }, [navigate]);
 
   return (
     <div className="App">
-      {/*1.  Header Component */}
-      <Header/>
-      <Routes>
-        {/* 2. Home Component */}
-        <Route path='/' element={<Home/>}/>
-        {/* 3. Sign Up Component */}
-        <Route path='/signup' element={<SignUp/>} />
-        {/* 4. Log In Component */}
-        <Route path='/login' element={<LogIn />} />
-        {/* 5. Welcome Component */}
-        <Route path='/welcome' element={<Welcome/>}/>
-        {/* 6. Questions Called Component */}
-        <Route path='/questions' element={<QuestionsCalled/>} />
-      </Routes>
-      {/* 7. Footer Component */}
-      <Footer/>
+        {/*1.  Header Component */}
+        <Header/>
+        <Routes>
+          {/* 2. Home Component */}
+          <Route path='/' element={<Home/>}/>
+          {/* 3. Sign Up Component */}
+          <Route path='/signup' element={<SignUp/>} />
+          {/* 4. Log In Component */}
+          <Route path='/login' element={<LogIn />} />
+          {/* 5. Welcome Component */}
+          <Route path='/welcome' element={<Welcome/>}/>
+          {/* 6. Questions Called Component */}
+          <Route path='/questions' element={<QuestionsCalled/>} />
+        </Routes>
+        {/* 7. Footer Component */}
+        <Footer/>
     </div>
   );
 }
 
 export default App;
+
+// export default function AppWithRouter(){
+//   return (
+//     <Router>
+//       <App />
+//     </Router>
+//   );
+// };
--- time-estimation-of-assignment/src/components/Header.jsx
+++ time-estimation-of-assignment/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from "../assets/FOCUS_FUTURE.png";
 import "./Header.css";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 export default function Header(props) {
     const navigate = useNavigate();
     
@@ -9,9 +9,13 @@ export default function Header(props) {
     <div className='header'>
       <img className='logo' src={logo} alt="logo" />
 
-      <button className='btn' onClick={()=> navigate(props.login ? "/login" : "/signup")}>
-        {props.login ? "Log In" : "Sign Up"}
-      </button>
+      {/* <button className='btn' onClick={()=> navigate('/signup')}>      */}
+      <Link to="/signup" className="btn">
+        Sign Up
+      </Link>
+        {/* navigate(props.login ? "/login" : "/signup")  */}
+        {/* {props.login ? "Log In" : "Sign Up"} */}
+      {/* </button> */}
     </div>
     // <Link to="/signup">Sign Up</Link>
   )
